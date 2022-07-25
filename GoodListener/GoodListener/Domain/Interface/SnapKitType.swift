e034e333e4ba441f2b312e1ed5b2361b44a0df8e
박천송 <qkrcjsthd@gmail.com>
2022-07-25 15:13:41 +0900

    Feat: 코드 일관성을 위한 인터페이스 작성 - ViewModel, SnapKit

---
+++ GoodListener/GoodListener/Domain/Interface/SnapKitType.swift
@@ -0,0 +1,13 @@
+//
+//  SnapKitType.swift
+//  GoodListener
+//
+//  Created by cheonsong on 2022/07/25.
+//
+
+import Foundation
+
+protocol SnapKitType {
+    func addComponents()
+    func setConstraints()
+}
---
+++ GoodListener/GoodListener/Domain/Interface/ViewModelType.swift
@@ -0,0 +1,23 @@
+//
+//  ViewModelType.swift
+//  GoodListener
+//
+//  Created by cheonsong on 2022/07/25.
+//
+
+import Foundation
+import RxSwift
+
+/**
+ 뷰모델 클래스를 작성시 해당 프로토콜을 채택해주세요
+ */
+protocol ViewModelType {
+    associatedtype Input
+    associatedtype Output
+    
+    var input: Input {get set}
+    var output: Output {get set}
+    var disposeBag: DisposeBag {get set}
+    
+    func transform(input: Input)-> Output
+}
