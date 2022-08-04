83ac78c41ef226fd3df22531eb8cbee27f0a5a0b
박천송 <qkrcjsthd@gmail.com>
2022-08-04 09:26:32 +0900

    Feat: SocketManager 구조 변경 - 소켓매니저 네이밍 변경 - SocketIO + RxSwift 로 구조 변경

--- GoodListener/GoodListener/Data/Network/SocketManager.swift
+++ GoodListener/GoodListener/Data/Network/SocketManager.swift
@@ -7,37 +7,96 @@
 
 import Foundation
 import SocketIO
+import RxSwift
+import RxCocoa
 
-class SocketIOManager: NSObject {
-    static let shared = SocketIOManager()
-    
-    var manager = SocketManager(socketURL: URL(string: "!!!URL!!!")!, config: [.log(true), .compress])
-    var socket: SocketIOClient!
-    override init() {
-        super.init()
-        socket = self.manager.socket(forNamespace: "/test") // "/test -> Room 서버쪽이랑 맞춰야함
+enum MySocketEvents: String {
+    // 사용자의 상태가 변경된 경우 -> Ex) 유저 닉네임 변경
+    case userUpdates
+    // 액션에 따른 UI 변경 및 기능 실행이 필요한 경우 -> Ex) 전화가 걸려온 상황에 전화 뷰를 올려줘야한다
+    case userActivities
+}
+
+final class GLSocketManager: NSObject {
+    struct Relays {
+        // Custom events:
+        /// User properties updated
+        let user = PublishRelay<[Any]>()
         
-        receiveEvent()
+        /// User activities added
+        let activities = PublishRelay<[Any]>()
+        
+        // Socket events:
+        /// Listen for socket connection changes.
+        /// Relays `true` on `connected`, `false` on `disconnected`.
+        let socketConnection = PublishRelay<Bool>()
     }
     
-    // socket 연결 시도
-    func establishConnection() {
+    static let shared = GLSocketManager()
+    
+    private var socketManager: SocketManager!
+    private let disposeBag = DisposeBag()
+    
+    // Room 정보 입력 필요
+    private var socket: SocketIOClient {
+        return socketManager.socket(forNamespace: "/my-namespace")
+    }
+    
+    // 공개적으로 접근 가능한 relays
+    let relays = Relays()
+    
+    // 싱글톤으로 start메서드를 실행해주세요 소켓이 연결됩니다
+    func start() {
+        //
+        guard socketManager == nil, let url = URL(string: "") else {
+            return
+        }
+        
+        // Initialize socket manager
+        socketManager = SocketManager(
+            socketURL: url,
+            config: [
+                .compress,
+                .log(true)
+            ]
+        )
+        
+        addListeners()
+        connect()
+    }
+    
+    func addListeners() {
+        // Custom Event를 MtsocketEvents 에 정의합니다
+        // 예시로 작성해 논것이며 MySocketEvents 열거형에 이벤트를 작성해야 합니다
+        socket.listen(event: MySocketEvents.userUpdates.rawValue, relay: relays.user)
+        socket.listen(event: MySocketEvents.userActivities.rawValue, relay: relays.activities)
+
+        // Connection 관리 relay.socketConnection을 구독하면 현재 소켓 연결 상태를 알 수 있다.
+        socket.listen(event: SocketClientEvent.connect.rawValue, result: true, relay: relays.socketConnection)
+        socket.listen(event: SocketClientEvent.disconnect.rawValue, result: false, relay: relays.socketConnection)
+    }
+    
+    func connect() {
         socket.connect()
     }
     
-    // socket 연결 종료
-    func closeConnection() {
+    func disconnect() {
         socket.disconnect()
     }
-    
-    // socket 이벤트 받는 함수
-    func receiveEvent() {
-        socket.on("test") { dataArray, ack in
-            print(dataArray)
+}
+
+extension SocketIOClient {
+    // 이벤트 발생 시 사용
+    func listen(event: String, relay: PublishRelay<[Any]>) {
+        on(event) { items, _ in
+            relay.accept(items)
         }
     }
     
-    func sendMessage(message: String, nickname: String) {
-
+    // 해당 이벤트가 발생했을때 원하는 결과값을 넘겨줄때 사용
+    func listen<T>(event: String, result: T, relay: PublishRelay<T>) {
+        on(event) { _, _ in
+            relay.accept(result)
+        }
     }
 }
