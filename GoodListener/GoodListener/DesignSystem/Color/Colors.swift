0f1d9c232fff7072d4d8d6811347c8d57509f47e
박천송 <qkrcjsthd@gmail.com>
2022-08-11 18:11:54 +0900

    Feat: Line Color 추가 - l1, l2

--- GoodListener/GoodListener/DesignSystem/Color/Colors.swift
+++ GoodListener/GoodListener/DesignSystem/Color/Colors.swift
@@ -45,4 +45,9 @@ extension UIColor {
     /// #95B593
     static var i4: UIColor { return #colorLiteral(red: 0.5843137255, green: 0.7098039216, blue: 0.5764705882, alpha: 1) }
     
+    
+    // MARK: LineColor - l
+    static var l1: UIColor { return #colorLiteral(red: 0.9592439532, green: 0.9656613469, blue: 0.9688369632, alpha: 1) }
+    
+    static var l2: UIColor { return #colorLiteral(red: 0.9560983777, green: 0.9625223279, blue: 0.965701282, alpha: 1) }
 }
