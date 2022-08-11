57e658faab5197114e13d04fab964129ef48677e
박천송 <qkrcjsthd@gmail.com>
2022-08-11 18:11:37 +0900

    Feat: NavigationView 수정 - backBtn, Title 추가

--- GoodListener/GoodListener/DesignSystem/NavigationView.swift
+++ GoodListener/GoodListener/DesignSystem/NavigationView.swift
@@ -13,10 +13,20 @@ enum NaviButtonCase: String {
     case notice = "ic_navi_noti"
     case setting = "ic_navi_setting_dark"
     case save
-    
+    case none
 }
 
 class NavigationView: UIView {
+    
+    let stackView = UIStackView().then {
+        $0.axis = .horizontal
+        $0.backgroundColor = .clear
+        $0.spacing = 13
+    }
+    
+    let backBtn = UIButton().then {
+        $0.setImage(UIImage(named: "ic_navi_back_btn"), for: .normal)
+    }
     
     let logo = UILabel().then {
         $0.text = "굿 리스너"
@@ -24,19 +34,37 @@ class NavigationView: UIView {
         $0.textColor = .f2
     }
     
-    let button = UIButton()
+    let title = UILabel().then {
+        $0.text = "title"
+        $0.font = FontManager.shared.notoSansKR(.regular, 18)
+        $0.textColor = .f2
+    }
+    
+    let rightBtn = UIButton()
 
     override init(frame: CGRect) {
         super.init(frame: frame)
-        addSubview(logo)
-        addSubview(button)
+        [stackView, logo, rightBtn].forEach { addSubview($0) }
+        [backBtn, title].forEach {
+            stackView.addArrangedSubview($0)
+            $0.isHidden = true
+        }
+        
+        stackView.snp.makeConstraints {
+            $0.left.equalToSuperview().inset(6)
+            $0.centerY.equalToSuperview()
+        }
+        
+        title.snp.makeConstraints {
+            $0.centerY.equalToSuperview()
+        }
         
         logo.snp.makeConstraints {
             $0.centerY.equalToSuperview()
             $0.left.equalToSuperview().inset(16)
         }
         
-        button.snp.makeConstraints {
+        rightBtn.snp.makeConstraints {
             $0.centerY.equalToSuperview()
             $0.right.equalToSuperview().inset(16)
         }
@@ -47,21 +75,23 @@ class NavigationView: UIView {
         
         switch type {
         case .notice:
-            button.setImage(UIImage(named: type.rawValue), for: .normal)
+            rightBtn.setImage(UIImage(named: type.rawValue), for: .normal)
         case .setting:
-            button.setImage(UIImage(named: type.rawValue), for: .normal)
+            rightBtn.setImage(UIImage(named: type.rawValue), for: .normal)
         case .save:
-            button.layer.cornerRadius = 10
-            button.setTitle("저장", for: .normal)
-            button.backgroundColor = .m1
-            button.setTitleColor(.white, for: .normal)
-            button.titleLabel?.font = FontManager.shared.notoSansKR(.bold, 13)
-            button.snp.remakeConstraints {
+            rightBtn.layer.cornerRadius = 10
+            rightBtn.setTitle("저장", for: .normal)
+            rightBtn.backgroundColor = .m1
+            rightBtn.setTitleColor(.white, for: .normal)
+            rightBtn.titleLabel?.font = FontManager.shared.notoSansKR(.bold, 13)
+            rightBtn.snp.remakeConstraints {
                 $0.centerY.equalToSuperview()
                 $0.right.equalToSuperview().inset(16)
-                $0.height.equalTo(35)
+                $0.height.equalTo(44)
                 $0.width.equalTo(50)
             }
+        case .none:
+            rightBtn.isHidden = true
         }
     }
     
