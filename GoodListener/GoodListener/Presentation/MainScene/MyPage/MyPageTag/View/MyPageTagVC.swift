976499e11065212785a296b4f92a0e53a087feb1
박천송 <qkrcjsthd@gmail.com>
2022-08-17 14:26:09 +0900

    Feat: TagCollectionView 높이 계산 함수 추가

--- GoodListener/GoodListener/Presentation/MainScene/MyPage/MyPageTag/View/MyPageTagVC.swift
+++ GoodListener/GoodListener/Presentation/MainScene/MyPage/MyPageTag/View/MyPageTagVC.swift
@@ -16,6 +16,10 @@ class MyPageTagVC: UIViewController, SnapKitType {
     var disposeBag = DisposeBag()
     static let borderColor = UIColor(red: 241/255, green: 243/255, blue: 244/255, alpha:1)
     
+    var ageList = ["10대", "20대", "30대", "40대 이상", "50대 이상"]
+    var sexList = ["남자", "여자"]
+    var jobList = ["기획", "디자인", "서버", "클라이언트"]
+    
     let navigationView = NavigationView(frame: .zero, type: .none).then {
         $0.logo.isHidden = true
         $0.backBtn.isHidden = false
@@ -28,20 +32,21 @@ class MyPageTagVC: UIViewController, SnapKitType {
         $0.axis = .vertical
     }
     
-    let ageTagView = TagView(frame: .zero, data: ["10대", "20대", "30대", "40대 이상", "50대 이상"]).then {
+    lazy var ageTagView = TagView(frame: .zero, data: ageList).then {
         $0.title.text = "나이"
     }
     
-    let sexTagView = TagView(frame: .zero, data: ["남자", "여자"]).then {
+    lazy var sexTagView = TagView(frame: .zero, data: sexList).then {
         $0.title.text = "성별"
     }
     
-    let jobTagView = TagView(frame: .zero, data: ["기획", "디자인", "서버", "클라이언트"]).then {
+    lazy var jobTagView = TagView(frame: .zero, data: jobList).then {
         $0.title.text = "직업"
     }
     
-    let tagView = TagView(frame: .zero, data: ["편안한"]).then {
+    lazy var tagView = TagView(frame: .zero, data: ["편안한", "잔인한", "행복한", "즐거운", "무서운"]).then {
         $0.title.text = "원하는 대화 분위기"
+        $0.line.isHidden = true
     }
     
     override func viewDidLoad() {
@@ -55,7 +60,6 @@ class MyPageTagVC: UIViewController, SnapKitType {
     
     func addComponents() {
         [navigationView, ageTagView, sexTagView, jobTagView, tagView].forEach { view.addSubview($0) }
-        
     }
     
     func setConstraints() {
@@ -68,19 +72,19 @@ class MyPageTagVC: UIViewController, SnapKitType {
         ageTagView.snp.makeConstraints {
             $0.top.equalTo(navigationView.snp.bottom)
             $0.left.right.equalToSuperview()
-            $0.height.equalTo(160)
+            $0.height.equalTo(tagCollectionViewHeight(list: ageList))
         }
         
         sexTagView.snp.makeConstraints {
             $0.top.equalTo(ageTagView.snp.bottom)
             $0.left.right.equalToSuperview()
-            $0.height.equalTo(113)
+            $0.height.equalTo(tagCollectionViewHeight(list: sexList))
         }
         
         jobTagView.snp.makeConstraints {
             $0.top.equalTo(sexTagView.snp.bottom)
             $0.left.right.equalToSuperview()
-            $0.height.equalTo(113)
+            $0.height.equalTo(tagCollectionViewHeight(list: jobList))
         }
         
         tagView.snp.makeConstraints {
@@ -96,5 +100,31 @@ class MyPageTagVC: UIViewController, SnapKitType {
                 self?.navigationController?.popViewController(animated: true)
             })
             .disposed(by: disposeBag)
+    }
+    
+    func tagCollectionViewHeight(list: [String])-> CGFloat {
+        let spacing: CGFloat = 8
+        var totalCellWidth: CGFloat = Const.padding * 2
+        let cellSpacing: CGFloat = 32
+        let screenWidth = UIScreen.main.bounds.width
+        var height: CGFloat = 113
+        
+        list.forEach { (text) in
+            let label = UILabel()
+            label.text = text
+            label.font = FontManager.shared.notoSansKR(.bold, 14)
+            label.sizeToFit()
+            Log.d("Text:: \(text)")
+            totalCellWidth += (label.frame.width + cellSpacing)
+            if totalCellWidth + spacing < screenWidth {
+                totalCellWidth += spacing
+            } else {
+                Log.d("TextPlus:: \(text)")
+                height += (38 + spacing)
+                totalCellWidth = Const.padding * 2
+            }
+        }
+
+        return height
     }
 }
