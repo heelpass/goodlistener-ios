3832c2dbc4e70e587d19e35f44e40b9692ae9fa8
박천송 <qkrcjsthd@gmail.com>
2022-08-19 13:00:58 +0900

    Feat: Login페이지 디자인 변경

--- GoodListener/GoodListener/Presentation/LoginScene/Login/View/LoginVC.swift
+++ GoodListener/GoodListener/Presentation/LoginScene/Login/View/LoginVC.swift
@@ -20,6 +20,7 @@ class LoginVC: UIViewController, SnapKitType {
     var disposeBag = DisposeBag()
     
     let titleLabel = UILabel().then {
+        $0.textColor = .white
         $0.text = "우리,\n같이 마음 편하게\n이야기해볼까요?"
         $0.textAlignment = .left
         $0.font = FontManager.shared.notoSansKR(.bold, 32)
@@ -30,24 +31,38 @@ class LoginVC: UIViewController, SnapKitType {
     
     let subtitleLabel = UILabel().then {
         $0.text = "내 마음 건강을 위한 매일 3분 보이스 루틴"
-        $0.textColor = .f4
+        $0.textColor = .white
         $0.font = FontManager.shared.notoSansKR(.regular, 14)
     }
     
-    let appleLoginBtn = ASAuthorizationAppleIDButton(authorizationButtonType: .signIn, authorizationButtonStyle: .black)
+    let buttonStackView = UIStackView().then {
+        $0.axis = .vertical
+        $0.spacing = 24
+        $0.distribution = .fillEqually
+    }
+    
+    let appleLoginBtn = UIButton().then {
+        $0.backgroundColor = .white
+        $0.setImage(UIImage(named: "appleLogoB"), for: .normal)
+        $0.title = "Apple로 로그인"
+        $0.titleColor = .f2
+        $0.font = FontManager.shared.notoSansKR(.bold, 16)
+        $0.contentHorizontalAlignment = .center
+        $0.imageEdgeInsets = .init(top: 0, left: 0, bottom: 0, right: 15) //<- 중요
+        $0.layer.cornerRadius = 24
+    }
     
     let nonLoginBtn = UIButton().then{
-        $0.setTitle("로그인 하지 않고 둘러보기", for: .normal)
-        $0.setTitleColor(.f3, for: .normal)
-        $0.titleLabel?.font = FontManager.shared.notoSansKR(.regular, 16)
-        $0.layer.cornerRadius = 6
-        $0.backgroundColor = .clear
-        $0.titleLabel?.textUnderLine(text: "로그인 하지 않고 둘러보기", range: ["로그인 하지 않고 둘러보기"])
+        $0.title = "이메일로 로그인"
+        $0.titleColor = .f2
+        $0.font = FontManager.shared.notoSansKR(.bold, 16)
+        $0.layer.cornerRadius = 24
+        $0.backgroundColor = .white
     }
     
     let termsOfServiceBtn = UIButton().then {
         $0.setTitle("이용약관 및 개인정보 취급방침", for: .normal)
-        $0.setTitleColor(.f4, for: .normal)
+        $0.titleColor = .white
         $0.titleLabel?.font = FontManager.shared.notoSansKR(.regular, 12)
         $0.backgroundColor = .clear
         $0.titleLabel?.textUnderLine(text: "이용약관 및 개인정보 취급방침", range: ["이용약관", "개인정보 취급방침"])
@@ -58,11 +73,12 @@ class LoginVC: UIViewController, SnapKitType {
         addComponents()
         setConstraints()
         bind()
-        view.backgroundColor = .white
+        view.backgroundColor = #colorLiteral(red: 0.1971904635, green: 0.2260227799, blue: 0.1979919374, alpha: 1)
     }
 
     func addComponents() {
-        [titleLabel, subtitleLabel, appleLoginBtn, nonLoginBtn, termsOfServiceBtn].forEach { view.addSubview($0) }
+        [titleLabel, subtitleLabel, buttonStackView, termsOfServiceBtn].forEach { view.addSubview($0) }
+        [appleLoginBtn, nonLoginBtn].forEach { buttonStackView.addArrangedSubview($0) }
     }
     
     func setConstraints() {
@@ -76,17 +92,16 @@ class LoginVC: UIViewController, SnapKitType {
             $0.left.equalToSuperview().inset(Const.padding)
         }
         
+        buttonStackView.snp.makeConstraints {
+            $0.left.right.equalToSuperview().inset(Const.padding)
+            $0.bottom.equalTo(termsOfServiceBtn.snp.top).offset(-39)
+        }
+        
         appleLoginBtn.snp.makeConstraints {
-            $0.bottom.equalTo(nonLoginBtn.snp.top).offset(-20)
-            $0.centerX.equalToSuperview()
-            $0.width.equalTo(Const.glBtnWidth)
             $0.height.equalTo(Const.glBtnHeight)
         }
         
         nonLoginBtn.snp.makeConstraints {
-            $0.bottom.equalTo(termsOfServiceBtn.snp.top).offset(-57)
-            $0.centerX.equalToSuperview()
-            $0.width.equalTo(Const.glBtnWidth)
             $0.height.equalTo(Const.glBtnHeight)
         }
         
