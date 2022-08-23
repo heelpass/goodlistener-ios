fc683571d813e731ecfa2f7f6e5c38d7d32334d1
박지영 <jyjoyp@gmail.com>
2022-08-23 14:29:10 +0900

    Feat: 매칭 후 대화 상태 UI 구현
    scroll 다는 거 필요해 보임

--- GoodListener/GoodListener/Presentation/JoinScene/Join/View/JoinMatchVC.swift
+++ GoodListener/GoodListener/Presentation/JoinScene/Join/View/JoinMatchVC.swift
@@ -20,7 +20,7 @@ class JoinMatchVC: UIViewController, SnapKitType {
     let disposeBag = DisposeBag()
     
     // 현재 매칭 화면 상태
-    var joinMatchState: JoinMatchState = .waiting
+    var joinMatchState: JoinMatchState = .matched
     
     //MARK: - 대기 중 State
     let waitingLbl = UILabel().then{
@@ -75,6 +75,106 @@ class JoinMatchVC: UIViewController, SnapKitType {
         $0.textColor = .f4
     }
     
+    //MARK: - 매칭 완료 State
+    let matchedTitle = UILabel().then {
+        $0.text = "새로운 리스너가 매칭되었어요!"
+        $0.font = FontManager.shared.notoSansKR(.bold, 20)
+        $0.textAlignment = .center
+        $0.textColor = .f2
+    }
+    
+    let matchedListenerLbl = UILabel().then{
+        $0.text = "리스너"
+        $0.font = FontManager.shared.notoSansKR(.bold, 16)
+        $0.textColor = .f3
+    }
+
+    let matchedImg = UIImageView().then{
+        $0.image = #imageLiteral(resourceName: "person")
+        $0.contentMode = .scaleAspectFill
+        $0.clipsToBounds = true
+    }
+    
+    let matchedNameLbl = UILabel().then {
+        $0.text = "명랑한 지윤이"
+        $0.font = FontManager.shared.notoSansKR(.bold, 18)
+        $0.textColor = .f3
+    }
+    
+    let matchedListenerStackView = UIStackView().then{
+        $0.axis = .horizontal
+        $0.backgroundColor = .clear
+    }
+    
+    let matchedGenderLbl = UILabel().then {
+        $0.text = "여성"
+        $0.font = FontManager.shared.notoSansKR(.regular, 14)
+        $0.textColor = .f2
+    }
+    
+    let lineOneView = UIView().then {
+        $0.backgroundColor = .f6
+    }
+    
+    let matchedAgeLbl = UILabel().then {
+        $0.text = "20대"
+        $0.font = FontManager.shared.notoSansKR(.regular, 14)
+        $0.textColor = .f2
+    }
+    
+    let lineTwoView = UIView().then{
+        $0.backgroundColor = .f6
+    }
+    
+    let matchedCharacterLblOne = UILabel().then{
+        $0.text = "밝음"
+        $0.font = FontManager.shared.notoSansKR(.regular, 14)
+        $0.textColor = .f2
+    }
+    
+    let lineThreeView = UIView().then{
+        $0.backgroundColor = .f6
+    }
+    
+    let matchedCharacterLblTwo = UILabel().then{
+        $0.text = "명랑"
+        $0.font = FontManager.shared.notoSansKR(.regular, 14)
+        $0.textColor = .f2
+    }
+    
+    let matchedIntroLbl = UILabel().then{
+        $0.text = "한마디"
+        $0.font = FontManager.shared.notoSansKR(.bold, 16)
+        $0.textColor = .f3
+    }
+    
+    let matchedIntrolDescriptionLbl = UILabel().then {
+        $0.text = "안녕하세요? 스피커님과 즐거운 대화를 해나가고 싶어요 일주일동안 잘 부탁드려요 안녕하세요? 스피커님과 즐거운..."
+        $0.textAlignment = .left
+        $0.numberOfLines = 3
+        $0.font = FontManager.shared.notoSansKR(.regular, 14)
+        $0.textColor = .f4
+        $0.lineBreakMode = .byTruncatingTail
+    }
+    
+    let matchedScheduleLbl = UILabel().then {
+        $0.text = "대화 시간"
+        $0.font = FontManager.shared.notoSansKR(.bold, 16)
+        $0.textColor = .f3
+    }
+    
+    let matchedTimeLbl = UILabel().then{
+        $0.text = "매일 오후 10:20"
+        $0.font = FontManager.shared.notoSansKR(.regular, 14)
+        $0.textColor = .f4
+    }
+    
+    let matchedDateLbl = UILabel().then {
+        $0.text = "2022.8.2 ~ 8.8 (7일간)"
+        $0.font = FontManager.shared.notoSansKR(.regular, 14)
+        $0.textColor = .f4
+    }
+  
     let confirmBtn = GLButton().then {
         $0.title = "확인"
     }
@@ -89,9 +189,14 @@ class JoinMatchVC: UIViewController, SnapKitType {
     }
     
     func addComponents() {
-        [waitingLbl, waitingImg, waitingdescriptionLbl, unableLbl, unableSubLbl, unableImg, unabledescriptionLbl, confirmBtn].forEach {
+        [waitingLbl, waitingImg, waitingdescriptionLbl, unableLbl, unableSubLbl, unableImg, unabledescriptionLbl, matchedTitle, matchedListenerLbl, matchedImg, matchedNameLbl, matchedListenerStackView, matchedIntroLbl, matchedIntrolDescriptionLbl, matchedScheduleLbl, matchedTimeLbl, matchedDateLbl, confirmBtn].forEach {
             view.addSubview($0)
         }
+        
+        [matchedGenderLbl, lineOneView, matchedAgeLbl, lineTwoView, matchedCharacterLblOne, lineThreeView, matchedCharacterLblTwo]
+            .forEach{
+                matchedListenerStackView.addArrangedSubview($0)
+            }
     }
     
     func setConstraints() {
@@ -131,6 +236,77 @@ class JoinMatchVC: UIViewController, SnapKitType {
             $0.centerX.equalToSuperview()
         }
         
+        matchedTitle.snp.makeConstraints{
+            $0.top.equalTo(view.safeAreaLayoutGuide).offset(67)
+            $0.centerX.equalToSuperview()
+        }
+        
+        matchedListenerLbl.snp.makeConstraints{
+            $0.top.equalTo(matchedTitle.snp.bottom).offset(47)
+            $0.left.equalToSuperview().offset(44)
+        }
+        
+        matchedImg.snp.makeConstraints{
+            $0.top.equalTo(matchedListenerLbl.snp.bottom).offset(17)
+            $0.left.equalToSuperview().offset(44)
+            $0.size.equalTo(CGSize(width: 72, height: 72))
+        }
+        
+        matchedNameLbl.snp.makeConstraints{
+            $0.top.equalTo(matchedListenerLbl.snp.bottom).offset(26)
+            $0.left.equalTo(matchedImg.snp.right).offset(12)
+        }
+        
+        matchedListenerStackView.snp.makeConstraints{
+            $0.top.equalTo(matchedNameLbl.snp.bottom).offset(8)
+            $0.left.equalTo(matchedImg.snp.right).offset(12)
+        }
+        
+        matchedListenerStackView.setCustomSpacing(5, after: matchedGenderLbl)
+        matchedListenerStackView.setCustomSpacing(5, after: lineOneView)
+        matchedListenerStackView.setCustomSpacing(5, after: matchedAgeLbl)
+        matchedListenerStackView.setCustomSpacing(5, after: lineTwoView)
+        matchedListenerStackView.setCustomSpacing(5, after: matchedCharacterLblOne)
+        matchedListenerStackView.setCustomSpacing(5, after: lineThreeView)
+        
+        lineOneView.snp.makeConstraints{
+            $0.size.equalTo(CGSize(width: 2, height: 15))
+        }
+        
+        lineTwoView.snp.makeConstraints{
+            $0.size.equalTo(CGSize(width: 2, height: 15))
+        }
+        
+        lineThreeView.snp.makeConstraints{
+            $0.size.equalTo(CGSize(width: 2, height: 15))
+        }
+        
+        matchedIntroLbl.snp.makeConstraints{
+            $0.top.equalTo(matchedImg.snp.bottom).offset(45)
+            $0.left.equalToSuperview().offset(44)
+        }
+        
+        matchedIntrolDescriptionLbl.snp.makeConstraints{
+            $0.top.equalTo(matchedIntroLbl.snp.bottom).offset(10)
+            $0.left.equalToSuperview().offset(44)
+            $0.right.equalToSuperview().offset(-44)
+        }
+        
+        matchedScheduleLbl.snp.makeConstraints{
+            $0.top.equalTo(matchedIntrolDescriptionLbl.snp.bottom).offset(44)
+            $0.left.equalToSuperview().offset(44)
+        }
+        
+        matchedTimeLbl.snp.makeConstraints{
+            $0.top.equalTo(matchedScheduleLbl.snp.bottom).offset(10)
+            $0.left.equalToSuperview().offset(44)
+        }
+        
+        matchedDateLbl.snp.makeConstraints{
+            $0.top.equalTo(matchedTimeLbl.snp.bottom)
+            $0.left.equalToSuperview().offset(44)
+        }
+        
         confirmBtn.snp.makeConstraints {
             $0.width.equalTo(Const.glBtnWidth)
             $0.height.equalTo(Const.glBtnHeight)
@@ -157,6 +333,16 @@ class JoinMatchVC: UIViewController, SnapKitType {
             unableSubLbl.isHidden = true
             unableImg.isHidden = true
             unabledescriptionLbl.isHidden = true
+            matchedTitle.isHidden = true
+            matchedListenerLbl.isHidden = true
+            matchedImg.isHidden = true
+            matchedNameLbl.isHidden = true
+            matchedListenerStackView.isHidden = true
+            matchedIntroLbl.isHidden = true
+            matchedIntrolDescriptionLbl.isHidden = true
+            matchedScheduleLbl.isHidden = true
+            matchedTimeLbl.isHidden = true
+            matchedDateLbl.isHidden = true
             confirmBtn.isHidden = true
             break
         case .unable:
@@ -167,9 +353,37 @@ class JoinMatchVC: UIViewController, SnapKitType {
             unableSubLbl.isHidden = false
             unableImg.isHidden = false
             unabledescriptionLbl.isHidden = false
+            matchedTitle.isHidden = true
+            matchedListenerLbl.isHidden = true
+            matchedImg.isHidden = true
+            matchedNameLbl.isHidden = true
+            matchedListenerStackView.isHidden = true
+            matchedIntroLbl.isHidden = true
+            matchedIntrolDescriptionLbl.isHidden = true
+            matchedScheduleLbl.isHidden = true
+            matchedTimeLbl.isHidden = true
+            matchedDateLbl.isHidden = true
             confirmBtn.isHidden = false
             break
         case .matched:
+            waitingLbl.isHidden = true
+            waitingImg.isHidden = true
+            waitingdescriptionLbl.isHidden = true
+            unableLbl.isHidden = true
+            unableSubLbl.isHidden = true
+            unableImg.isHidden = true
+            unabledescriptionLbl.isHidden = true
+            matchedTitle.isHidden = false
+            matchedListenerLbl.isHidden = false
+            matchedImg.isHidden = false
+            matchedNameLbl.isHidden = false
+            matchedListenerStackView.isHidden = false
+            matchedIntroLbl.isHidden = false
+            matchedIntrolDescriptionLbl.isHidden = false
+            matchedScheduleLbl.isHidden = false
+            matchedTimeLbl.isHidden = false
+            matchedDateLbl.isHidden = false
+            confirmBtn.isHidden = false
             break
         }
     }
--- GoodListener/GoodListener/Presentation/MainScene/Home/View/HomeVC.swift
+++ GoodListener/GoodListener/Presentation/MainScene/Home/View/HomeVC.swift
@@ -9,6 +9,7 @@ import UIKit
 import RxSwift
 
 // 신청 전, 매칭 후 2가지 상태
+// TODO: 연장 버튼 눌렀을 때 추가 - 기획 답변
 enum homeState {
     case join
     case matched
