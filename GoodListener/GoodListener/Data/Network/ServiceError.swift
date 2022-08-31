6e26dbc83854befe1c4d60452a51b99807607dd4
박천송 <qkrcjsthd@gmail.com>
2022-08-31 14:39:52 +0900

    [#27]Feat: ServiceError 추가

--- GoodListener/GoodListener/Data/Network/ServiceError.swift
+++ GoodListener/GoodListener/Data/Network/ServiceError.swift
@@ -6,3 +6,24 @@
 //
 
 import Foundation
+import Moya
+
+// back-end 팀과 정의한 에러 내용
+enum ServiceError: Error {
+    case moyaError(MoyaError)
+    case invalidResponse(responseCode: Int, message: String)
+    case tokenExpired
+}
+
+extension ServiceError: LocalizedError {
+    var errorDescription: String? {
+        switch self {
+        case .moyaError(let moyaError):
+            return moyaError.localizedDescription
+        case let .invalidResponse(_, message):
+            return message
+        case .tokenExpired:
+            return "AccessToken Expired"
+        }
+    }
+}
