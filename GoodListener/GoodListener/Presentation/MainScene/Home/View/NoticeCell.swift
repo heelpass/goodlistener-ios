ad9ef1ff2005a1095eb4a96ca19c27b072da2b1b
박지영 <jyjoyp@gmail.com>
2022-09-03 21:27:13 +0900

    Feat: 알림 있을 때 UI 구현

---
+++ GoodListener/GoodListener/Presentation/MainScene/Home/View/NoticeCell.swift
@@ -0,0 +1,81 @@
+//
+//  NoticeCell.swift
+//  GoodListener
+//
+//  Created by Jiyoung Park on 2022/09/03.
+//
+
+import UIKit
+
+class NoticeCell: UICollectionViewCell {
+    static let identifier = "Noticecell"
+    
+    let profileImg = UIImageView().then{
+        $0.image = #imageLiteral(resourceName: "person")
+        $0.contentMode = .scaleAspectFill
+    }
+
+    let nickNameLbl = UILabel().then {
+        $0.text = "명랑한 지윤이"
+        $0.textColor = .f3
+        $0.font = FontManager.shared.notoSansKR(.bold, 14)
+        $0.textAlignment = .center
+    }
+    
+    let timeLbl = UILabel().then {
+        $0.text = "매일 오후 10:20"
+        $0.textColor = .f3
+        $0.font = FontManager.shared.notoSansKR(.regular, 14)
+        $0.textAlignment = .center
+    }
+    
+    let periodLbl = UILabel().then {
+        $0.text = "2022.8.2 ~ 8.8(7일간)"
+        $0.textColor = .f3
+        $0.font = FontManager.shared.notoSansKR(.regular, 14)
+        $0.textAlignment = .center
+    }
+    
+    override init(frame: CGRect){
+        super.init(frame: frame)
+        addComponents()
+        setConstraints()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+
+    }
+    
+    func addComponents() {
+        [profileImg, nickNameLbl, timeLbl, periodLbl].forEach{
+            contentView.addSubview($0)
+        }
+    }
+    
+    func setConstraints() {
+        profileImg.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(20)
+            $0.left.equalToSuperview().offset(20)
+            $0.size.equalTo(80)
+        }
+        
+        nickNameLbl.snp.makeConstraints{
+            $0.top.equalToSuperview().offset(20)
+            $0.left.equalTo(profileImg.snp.right).offset(20)
+        }
+        
+        timeLbl.snp.makeConstraints{
+            $0.top.equalTo(nickNameLbl.snp.bottom).offset(12)
+            $0.left.equalTo(profileImg.snp.right).offset(20)
+        }
+        
+        periodLbl.snp.makeConstraints{
+            $0.top.equalTo(timeLbl.snp.bottom)
+            $0.left.equalTo(profileImg.snp.right).offset(20)
+        }
+
+    }
+}
+
+
--- GoodListener/GoodListener/Presentation/MainScene/Home/View/NoticeVC.swift
+++ GoodListener/GoodListener/Presentation/MainScene/Home/View/NoticeVC.swift
@@ -13,13 +13,12 @@ enum noticeState {
     case notice
 }
 
-
 class NoticeVC: UIViewController, SnapKitType {
     
     weak var coordinator: HomeCoordinating?
     let disposeBag = DisposeBag()
     
-    var noticeState: noticeState = .none
+    var noticeState: noticeState = .notice
     
     let navigationView = NavigationView(frame: .zero, type: .none).then {
         $0.logo.isHidden = true
@@ -51,6 +50,13 @@ class NoticeVC: UIViewController, SnapKitType {
         $0.font = FontManager.shared.notoSansKR(.bold, 16)
     }
     
+    //알림 있을 때 
+    let RecordBgView : UICollectionView = {
+        let view = UICollectionView(frame: .zero, collectionViewLayout: UICollectionViewFlowLayout())
+        view.backgroundColor = .m5
+        view.showsVerticalScrollIndicator = false
+        return view
+    }()
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -62,9 +68,13 @@ class NoticeVC: UIViewController, SnapKitType {
     }
     
     func addComponents() {
-        [navigationView, bellImg, nonetitleLbl, noneContentLbl, noneSettingLbl].forEach{
+        [navigationView, RecordBgView, bellImg, nonetitleLbl, noneContentLbl, noneSettingLbl].forEach{
             view.addSubview($0)
         }
+        
+        RecordBgView.register(NoticeCell.self, forCellWithReuseIdentifier:NoticeCell.identifier)
+        RecordBgView.delegate = self
+        RecordBgView.dataSource = self
     }
     
     func setConstraints() {
@@ -95,6 +105,13 @@ class NoticeVC: UIViewController, SnapKitType {
             $0.centerX.equalToSuperview()
         }
         
+        RecordBgView.snp.makeConstraints{
+            $0.top.equalTo(navigationView.snp.bottom)
+            $0.left.equalToSuperview()
+            $0.right.equalToSuperview()
+            $0.bottom.equalTo(view.safeAreaLayoutGuide.snp.bottom).offset(-20)
+        }
+        
     }
     
     func bind() {
@@ -112,13 +129,48 @@ class NoticeVC: UIViewController, SnapKitType {
             nonetitleLbl.isHidden = false
             noneContentLbl.isHidden = false
             noneSettingLbl.isHidden = false
+            RecordBgView.isHidden = true
             break
         case .notice:
             bellImg.isHidden = true
             nonetitleLbl.isHidden = true
             noneContentLbl.isHidden = true
             noneSettingLbl.isHidden = true
+            RecordBgView.isHidden = false
             break
         }
+    }
+}
+
+
+extension NoticeVC: UICollectionViewDataSource {
+    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
+        return 5
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
+        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: NoticeCell.identifier, for: indexPath) as? NoticeCell else { fatalError() }
+        cell.backgroundColor = UIColor(hex: "F7FFF2")
+        cell.layer.cornerRadius = 10
+        return cell
+    }
+}
+
+extension NoticeVC: UICollectionViewDelegateFlowLayout {
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
+        return 20
+    }
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, insetForSectionAt section: Int) -> UIEdgeInsets {
+        return UIEdgeInsets(top: 10, left: 0, bottom: 20, right: 0)
+    }
+    
+    
+    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
+        let width = view.frame.size.width - Const.padding * 2
+        return CGSize(
+            width: width,
+            height: width * 0.3
+        )
     }
 }
