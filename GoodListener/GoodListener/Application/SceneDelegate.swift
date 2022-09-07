ed017ae8868124d2cf4122bcccb3f75f28433a30
박천송 <qkrcjsthd@gmail.com>
2022-09-07 14:06:16 +0900

    Feat: DBManager추가 - savePushData 로직 이동
    Former-commit-id: 2050f8006ad5dc180e9d6c836d9b2a76e344d835

--- GoodListener/GoodListener/Application/SceneDelegate.swift
+++ GoodListener/GoodListener/Application/SceneDelegate.swift
@@ -55,7 +55,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
             UIApplication.shared.applicationIconBadgeNumber = 0
         }
         
-        savePushData()
+        DBManager.shared.savePushData()
     }
     
     func sceneWillResignActive(_ scene: UIScene) {
@@ -72,50 +72,6 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Called as the scene transitions from the foreground to the background.
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
-    }
-    
-    func dateConverter()-> String {
-        let dateFormatter = DateFormatter()
-        dateFormatter.dateFormat = "yyyy.MM.dd"
-        return dateFormatter.string(from: Date())
-    }
-    
-    public func savePushData() {
-        if let userDefault = UserDefaults(suiteName: "group.com.heelpass.good-listener") {
-            if let pushData = userDefault.value(forKey: "pushData") as? [String] {
-                
-                pushData.forEach { pushType in
-                    let pushModel = PushModel()
-                    
-                    // PushModel PK값 지정
-                    pushModel.id = UserDefaultsManager.shared.pushCnt + 1
-                    UserDefaultsManager.shared.pushCnt = pushModel.id
-                    pushModel.date = dateConverter()
-                    // PushModel 나머지 value 지정
-                    switch pushType {
-                    case "call":
-                        pushModel.title = "굿리스너 통화 안내"
-                        pushModel.body = "[스피커닉네임]님이 통화가 걸었어요~"
-                    case "cancel":
-                        pushModel.title = "굿리스너 취소 안내"
-                        pushModel.body = "[스피커닉네임]님이 통화가 취소됐어요"
-                    case "remain5":
-                        pushModel.title = "굿리스너 통화 안내"
-                        pushModel.body = "5분뒤 [스피커닉네임]과 통화 예정이에요"
-                    default:
-                        break
-                    }
-                    
-                    // realm DB에 저장
-                    let realm = try! Realm()
-                    try! realm.write {
-                        realm.add(pushModel)
-                    }
-                }
-
-                userDefault.set(nil, forKey: "pushData")
-            }
-        }
     }
     
 }
