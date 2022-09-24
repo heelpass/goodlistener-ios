61a44d5a693638df429bf823a87b00dbce86f269
jyjoyp <jyjoyp@gmail.com>
2022-09-24 22:32:54 +0900

    Feat: 리스너용 Maincoordinator 재배치

--- GoodListener/GoodListener/Presentation/MainScene/Flow/MainCoordinator.swift
+++ GoodListener/GoodListener/Presentation/MainScene/Flow/MainCoordinator.swift
@@ -33,7 +33,7 @@ class MainCoordinator: CoordinatorType {
     var tabBarController: CustomTabBarController
     
     // 유저 타입도 AppCoordinator에서 주입해줘야 합니다
-    var userType: UserType = .speaker
+    var userType: UserType = .listener
     
     init(navigationController: UINavigationController) {
         self.navigationController = navigationController
@@ -108,34 +108,55 @@ class MainCoordinator: CoordinatorType {
     
     // 리스너인경우 리스너 탭바 실행
     func listenerStart() {
-        let home = HomeCoordinator(coordinating: self)
-        let applicant = ApplicantCoordinator(coordinating: self)
-        let record = RecordCoordinator(coordinating: self)
-        let myPage = MyPageCoordinator(coordinating: self)
+        
+            // 1️⃣ 탭별로 코디네이터 객체를 생성해주세요!
+            // navigationController는 새로운 인스턴스로 생성해주세요
+            // coordinating의존성을 주입해 주세요!
+            let applicant = ApplicantCoordinator(coordinating: self)
+            let record = RecordCoordinator(coordinating: self)
+            let myPage = MyPageCoordinator(coordinating: self)
 
-
-        home.start()
+            
+            // 2️⃣ 탭바 컨트롤러에 하위 뷰컨트롤러를 추가해주세요!
         applicant.start()
-        record.start()
-        myPage.start()
+            record.start()
+            myPage.start()
 
+            
+            // 3️⃣ MainCoordrinator - childCoordinators 에 각각의 코디네이터를 추가해주세요!
+            // 추가하지 않으면 메모리에 남지않고 사라져버립니다
+            // ❗️주의 : 뷰컨을 내려주기전에 childCoordinators에서 해당 코디네이터를 remove 시켜주세요 remove 시키지 않으면 메모리 누수가 발생합니다!
+            // CoordinatorType.childDidFinish(child: CoordinatorType) 함수를 사용하면 childCoordinators에서 원하는 Coodinator만 제거가 가능합니다!
+            childCoordinators.append(applicant)
+            childCoordinators.append(record)
+            childCoordinators.append(myPage)
 
-        childCoordinators.append(home)
-        childCoordinators.append(applicant)
-        childCoordinators.append(record)
-        childCoordinators.append(myPage)
+            let fontAttributes = [NSAttributedString.Key.font: FontManager.shared.notoSansKR(.bold, 12)]
+            
+            applicant.navigationController.tabBarItem = UITabBarItem(title: "홈", image: UIImage(named: "ic_gnb_home_off"), tag: 0)
+            applicant.navigationController.tabBarItem.selectedImage = UIImage(named: "ic_gnb_home_on")
+            applicant.navigationController.tabBarItem.setTitleTextAttributes(fontAttributes, for: .normal)
+            applicant.navigationController.tabBarItem.setTitleTextAttributes(fontAttributes, for: .selected)
+            
+            record.navigationController.tabBarItem = UITabBarItem(title: "대화 기록", image: UIImage(named: "ic_gnb_history_off"), tag: 1)
+            record.navigationController.tabBarItem.selectedImage = UIImage(named: "ic_gnb_history_on")
+            record.navigationController.tabBarItem.setTitleTextAttributes(fontAttributes, for: .normal)
+            record.navigationController.tabBarItem.setTitleTextAttributes(fontAttributes, for: .selected)
+            
+            myPage.navigationController.tabBarItem = UITabBarItem(title: "MY", image: UIImage(named: "ic_gnb_my_off"), tag: 2)
+            myPage.navigationController.tabBarItem.selectedImage = UIImage(named: "ic_gnb_my_on")
+            myPage.navigationController.tabBarItem.setTitleTextAttributes(fontAttributes, for: .normal)
+            myPage.navigationController.tabBarItem.setTitleTextAttributes(fontAttributes, for: .selected)
+            
+            tabBarController.tabBar.backgroundColor = .m5
+            tabBarController.tabBar.tintColor = .m1
+            tabBarController.tabBar.unselectedItemTintColor = .f5
 
-        
-        home.navigationController.tabBarItem = UITabBarItem(title: "홈", image: nil, tag: 0)
-        applicant.navigationController.tabBarItem = UITabBarItem(title: "대화 신청자", image: nil, tag: 1)
-        record.navigationController.tabBarItem = UITabBarItem(title: "통화 기록", image: nil, tag: 2)
-        myPage.navigationController.tabBarItem = UITabBarItem(title: "마이 페이지", image: nil, tag: 3)
-
-
-        tabBarController.viewControllers = [home.navigationController, applicant.navigationController, record.navigationController, myPage.navigationController]
-        
-        tabBarController.modalPresentationStyle = .fullScreen
-        navigationController.present(tabBarController, animated: true)
+            // 4️⃣ TabBarController.viewControllers에 각각코디네이터의 navigationController을 추가해주세요!
+            tabBarController.viewControllers = [applicant.navigationController, record.navigationController, myPage.navigationController]
+            
+            tabBarController.modalPresentationStyle = .fullScreen
+            navigationController.present(tabBarController, animated: true)
     }
 }
 
