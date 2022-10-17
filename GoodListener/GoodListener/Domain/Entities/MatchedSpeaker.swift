c0c17d0c33af5705dd9c274c6fd57198eba67ed0
박지영 <jyjoyp@gmail.com>
2022-10-17 14:11:16 +0900

    Feat: 나의 리스너 API에 codingkey 추가

--- GoodListener/GoodListener/Domain/Entities/MatchedSpeaker.swift
+++ GoodListener/GoodListener/Domain/Entities/MatchedSpeaker.swift
@@ -15,9 +15,30 @@ struct MatchedSpeaker: Codable {
     var nickname: String
     var description: String
     var meetingTime: String
+    
+    enum CodingKeys: String, CodingKey {
+        case channel
+        case channelId
+        case listenerId
+        case speaker
+        case nickname
+        case description
+        case meetingTime
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.channel = try container.decode(String.self, forKey: .channel)
+        self.channelId = try container.decode(Int.self, forKey: .channelId)
+        self.listenerId = try container.decode(Int.self, forKey: .listenerId)
+        self.speaker = try container.decode(SpeakerInfo.self, forKey: .speaker)
+        self.nickname = try container.decode(String.self, forKey: .nickname)
+        self.description = try container.decode(String.self, forKey: .description)
+        self.meetingTime = try container.decode(String.self, forKey: .meetingTime)
+    }
 }
 
-struct SpeakerInfo : Codable {
+struct SpeakerInfo: Codable {
     var id: Int
     var nickName: String
     var gender: String
@@ -25,4 +46,26 @@ struct SpeakerInfo : Codable {
     var job: String
     var description: String
     var profileImg: Int
+    
+    enum CodingKeys: String, CodingKey{
+        case id
+        case nickName
+        case gender
+        case ageRange
+        case job
+        case description
+        case profileImg
+    }
+    
+    init(from decoder: Decoder) throws {
+        let container = try decoder.container(keyedBy: CodingKeys.self)
+        self.id = (try? container.decode(Int.self, forKey: .id)) ?? 0
+        self.nickName = (try? container.decode(String.self, forKey: .nickName)) ?? ""
+        self.gender = (try? container.decode(String.self, forKey: .gender)) ?? ""
+        self.ageRange = (try? container.decode(String.self, forKey: .ageRange)) ?? ""
+        self.job = (try? container.decode(String.self, forKey: .job)) ?? ""
+        self.description = (try? container.decode(String.self, forKey: .description)) ?? ""
+        self.profileImg = (try? container.decode(Int.self, forKey: .profileImg)) ?? 0
+    }
+    
 }
